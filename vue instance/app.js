a4018944d0eca87ee34763e4a9ba90b7ff6f254e
MirajHoque <a.h.miraj2031@gmail.com>
2020-07-20 23:02:11 +0600

    view instance
    vue.js

---
+++ vue instance/app.js
@@ -0,0 +1,10 @@
+new Vue({
+    //vue instance
+    //it's role is to control either whole part or certain part of our application
+    el: '#vue-app',
+    //element:which elements is control this vue instances
+    data: {
+        //where we store all of our data for vue instance
+        name: 'Miraj'
+    }
+})

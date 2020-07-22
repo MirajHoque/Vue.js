21a4b9b888e274b37c545a4b89f57594c9f7d92b
MirajHoque <a.h.miraj2031@gmail.com>
2020-07-22 23:12:44 +0600

    events
    vue.js

---
+++ events/app.js
@@ -0,0 +1,27 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        age: 25,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        add: function(inc) {
+            this.age += inc;
+        },
+
+        subtract: function(dec) {
+            this.age -= dec;
+        },
+        updateXY: function(event) {
+            //whenever we perform event we got a event object
+            //console.log(event);
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+
+        }
+
+
+    }
+
+});

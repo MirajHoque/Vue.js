51613a5a65dea9a3cccf516645f1900007ed1bd3
MirajHoque <a.h.miraj2031@gmail.com>
2020-07-24 23:40:18 +0600

    keyboardEvents
    vue.js

---
+++ keyboardEvents/app.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+
+    },
+    methods: {
+        logName: function() {
+            console.log("you entered your name");
+        },
+        logAge: function() {
+            console.log("you entered your name");
+        }
+    }
+
+})

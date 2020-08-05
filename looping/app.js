d2cbaf20ccfec30cc5e8640e451f0822a2828864
MirajHoque <a.h.miraj2031@gmail.com>
2020-08-05 23:25:58 +0600

    loops
    Vue.js

---
+++ looping/app.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        children: ['Mita', 'Miraj', 'Mila'],
+        parents: [
+            { name: 'Saleha Akther', age: 50 },
+            { name: 'Abdul Hoque', age: 55 }
+        ]
+
+    },
+    methods: {
+
+    }
+})
